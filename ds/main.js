0f439197a301157dfbc03009489eb231a65e9bb0
Cole Wilson <colewilson.main@gmail.com>
2024-09-26 16:31:02 -0700

    asd

--- ds/main.js
+++ ds/main.js
@@ -1,3 +1,4 @@
+var GUI = lil.GUI;
 document.getElementById("dashboard").dataset.view = localStorage.getItem("dashboardtype") || "simulation";
 document.getElementById("dashboardview").value = localStorage.getItem("dashboardtype") || "simulation";
 document.getElementById("dashboardview").onchange = (e) => {
@@ -15,9 +16,35 @@ function setEnabled(value) {
 
 
 
+// db_gui = new GUI();
+db_folders = {};
+db_keys = [];
+
 function update() {
 	let parent_el = document.getElementById("networktable");
 	parent_el.innerText = JSON.stringify(network, null, 2);
+
+
+	// // let db = document.getElementById("dashboardpanel");
+	// Object.keys(network).forEach(key=>{
+	// 	if (!(db_keys.includes(key))) {
+	// 		db_keys.push(key)
+	// 		split = key.split("/");
+	// 		try {
+	// 			if (split.length > 1) {
+	// 				if (!(split[0] in db_folders)) {
+	// 					db_folders[split[0]] = db_gui.addFolder(split[0])
+	// 				}
+	// 				db_folders[split[0]].add(network, key).name(split[1]);
+	// 			} else {
+	// 				db_gui.add(network, key).name(key)
+	// 			}
+	// 		} catch {
+
+	// 		}
+	// 	}
+	// })
+
 }
 setInterval(update, 100)
 
